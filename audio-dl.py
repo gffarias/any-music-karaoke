49220a17f276f87c45006f3d62faeb4302c51c5d
Gustavo F. de Farias <gff@cin.ufpe.br>
2021-11-23 20:49:35 -0300

    absolute path fixed to relative path

--- audio-dl.py
+++ audio-dl.py
@@ -4,5 +4,5 @@ yt = YouTube(url)
 st = yt.streams.filter(only_audio=True).order_by(attribute_name='abr').last()
 video_codec, audio_codec = st.parse_codecs()
 filename = f'{yt.video_id}.{audio_codec}'
-st.download(output_path='/home/gffarias/Repositories/any-music-karaoke/cache/download', filename=filename)
+st.download(output_path='cache/download', filename=filename)
 print(filename)
