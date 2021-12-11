1c36f5da1080733eb1e9281eaeb42dc7553eb8df
Gustavo F. de Farias <gff@cin.ufpe.br>
2021-12-11 02:06:44 -0300

    Backend pronto

--- audio-dl.py
+++ audio-dl.py
@@ -2,7 +2,8 @@ url = input()
 from pytube import YouTube
 yt = YouTube(url)
 st = yt.streams.filter(only_audio=True).order_by(attribute_name='abr').last()
-video_codec, audio_codec = st.parse_codecs()
-filename = f'{yt.video_id}.{audio_codec}'
-st.download(output_path='public/cache/download', filename=filename)
+ext = st.mime_type.split('/')[1]
+# title, artist = yt.description.split('\n')[2].split(' · ')
+filename = f'{yt.video_id}.{ext}'
+st.download(output_path='public/assets/download', filename=filename)
 print(filename)
--- core.sh
+++ core.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 FILENAME=$(echo $1 | python audio-dl.py)
-spleeter separate -o "public/cache/" -f {instrument}/{filename}.{codec} -c mp3 "public/cache/download/$FILENAME" > /dev/null
-echo "cache/accompaniment/${FILENAME%%.*}.mp3"
+spleeter separate -o "public/assets/" -f {instrument}/{filename}.{codec} -c mp3 "public/assets/download/$FILENAME" > /dev/null
+echo "assets/accompaniment/${FILENAME%%.*}.mp3"
